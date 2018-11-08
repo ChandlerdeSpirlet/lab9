98480e06711f79582e7e181382eda31b500971a8
Chandler de Spirlet <chandler.despirlet@icloud.com>
2018-11-08 16:39:06 -0700

    hihi

--- database.js
+++ database.js
@@ -1,7 +1,7 @@
 var pgp = require('pg-promise')();
 
-var​ dbConfig = process.env.DATABASE_URL;
-
+//var​ dbConfig = process.env.DATABASE_URL;
+var dbConfig = process.env.DATABASE_URL;
 var db = pgp(dbConfig);
 
 module.exports = db;
